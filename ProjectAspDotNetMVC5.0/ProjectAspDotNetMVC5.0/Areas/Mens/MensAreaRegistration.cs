1bb27642e55438d00dd49be0c49c1fad7d6f2ede
Mohammad Abu Naser <mdabunaser@live.com>
2022-07-05 15:40:20 -0600

    Area added

---
+++ ProjectAspDotNetMVC5.0/ProjectAspDotNetMVC5.0/Areas/Mens/MensAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ProjectAspDotNetMVC5._0.Areas.Mens
+{
+    public class MensAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Mens";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Mens_default",
+                "Mens/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ProjectAspDotNetMVC5.0/ProjectAspDotNetMVC5.0/Areas/Womens/WomensAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ProjectAspDotNetMVC5._0.Areas.Womens
+{
+    public class WomensAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Womens";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Womens_default",
+                "Womens/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
